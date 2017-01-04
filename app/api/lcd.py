a625e7c8f43147c3aca6f94c83fc438db48e4200
Fredrik Steen <fredrik.steen@redpill-linpro.com>
2017-01-04 09:26:48 +0100

    LCD autorefresh / lcd api added
    This commit enables autorefresh (every 5s) of the lcd's on the frontpage.
    
    It also addes an api endpoint:
    
    /api/lcd - returns a json with all lcds
    /api/lcd/<name> - returns a json with the <name> data
    
    The listing of lcd's has moved from django templates to javascript
    and is using vuejs for templating.

---
+++ app/api/lcd.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render_to_response, redirect
+from django.http import JsonResponse
+
+from app.models import BrewPiDevice
+
+def getLCDs(req):
+    ret = []
+    all_devices = BrewPiDevice.objects.all()
+    for dev in all_devices:
+        ret.append({"device_name": dev.device_name, "lcd_data": dev.read_lcd()})
+    return JsonResponse(ret, safe=False, json_dumps_params={'indent': 4})
+
+def getLCD(req, device_name):
+    device = BrewPiDevice.objects.get(device_name=device_name)
+    return JsonResponse(device.read_lcd(), safe=False, json_dumps_params={'indent': 4})
---
+++ app/static/js/lcd.js
@@ -0,0 +1,24 @@
+var vm = new Vue({
+  el: '#lcdapp',
+  data: {
+    lcds: ''
+  },
+  mounted: function () {
+    this.getLCDs()
+    window.setInterval(() => {
+      this.getLCDs()
+    }, 5000)
+  },
+  methods: {
+      getLCDs: function() {
+        var xhr = new XMLHttpRequest()
+        var self = this
+        xhr.open('GET', '/api/lcd/')
+        xhr.onload = function () {
+          self.lcds = JSON.parse(xhr.responseText)
+          console.log(self.lcds)
+        }
+        xhr.send()
+      }
+  }
+})
