89bc61b29b9c9c7db38a6ca1f70137efaf8cc5b9
Thorrak <thorrak@excelinspace.com>
2017-02-04 22:30:01 -0500

    Reorganizing script locations

--- utils/make_secretsettings.sh
+++ utils/make_secretsettings.sh
@@ -4,4 +4,4 @@
 # SECRET_KEY is a 50 character alphanumeric string that is used within Django
 SECRET_KEY_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 50 | head -n 1)
 
-echo "SECRET_KEY = '${SECRET_KEY_STRING}'" >> "brewpi_django/secretsettings.py"
+echo "SECRET_KEY = '${SECRET_KEY_STRING}'" >> "../brewpi_django/secretsettings.py"
