2c37c485da4e6828ff1f82d10f3fbe33bc615a93
Thorrak <thorrak@excelinspace.com>
2017-02-18 10:19:50 -0500

    Update to capture branch name in git integration

--- app/git_integration.py
+++ app/git_integration.py
@@ -7,7 +7,10 @@ def local_and_remote_are_at_same_commit(repo, remote):
     local_commit = repo.commit()
     remote_fetch = remote.fetch()
     remote_commit = remote_fetch[0].commit
-    return local_commit.hexsha == remote_commit.hexsha
+    # We don't want to upgrade if the local commit is newer than the remote commit (IE - we're doing development
+    # on the local copy)
+    return remote_commit.committed_datetime >= local_commit.committed_datetime
+    # return local_commit.hexsha == remote_commit.hexsha
 
 
 def app_is_current():
@@ -24,5 +27,7 @@ def get_local_remote_commit_info():
     local_commit = local_repo.commit()
     remote_commit = remote_repo.fetch()[0].commit
 
-    commit_dict = {'local': local_commit, 'remote': remote_commit}
+    local_branch = local_repo.active_branch.name
+
+    commit_dict = {'local': local_commit, 'remote': remote_commit, 'local_branch': local_branch, 'remote_branch': ''}
     return commit_dict
