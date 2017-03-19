06d4f85b852c182cab2606b2bbf030b00fc0db10
Thorrak <thorrak@excelinspace.com>
2017-03-19 17:58:12 -0400

    Refactor upgrade.sh to log to a file (Fixes #120)

--- utils/upgrade.sh
+++ utils/upgrade.sh
@@ -4,11 +4,33 @@
 BRANCH="master"
 SILENT=0
 
+# Colors (for printinfo/error/warn below)
+green=$(tput setaf 76)
+red=$(tput setaf 1)
+tan=$(tput setaf 3)
+reset=$(tput sgr0)
+
+
 # Help text
 function usage() {
     echo "Usage: $0 [-h] [-b <branch name>]" 1>&2
     exit 1
 }
+
+printinfo() {
+  printf "::: ${green}%s${reset}\n" "$@"
+}
+
+
+printwarn() {
+ printf "${tan}*** WARNING: %s${reset}\n" "$@"
+}
+
+
+printerror() {
+ printf "${red}*** ERROR: %s${reset}\n" "$@"
+}
+
 
 while getopts ":b:sh" opt; do
   case ${opt} in
@@ -33,42 +55,46 @@ done
 
 shift $((OPTIND-1))
 
-echo "Triggering upgrade from branch ${BRANCH}"
+
+exec > >(tee -i upgrade.log)
+
+
+printinfo "Triggering upgrade from branch ${BRANCH}"
 # First, launch the virtualenv
 source ~/venv/bin/activate  # Assuming the directory based on a normal install with Fermentrack-tools
 
 # Given that this script can be called by the webapp proper, give it 2 seconds to finish sending a reply to the
 # user if he/she initiated an upgrade through the webapp.
-echo "Waiting 2 seconds for Fermentrack to send updates if triggered from the web..."
+printinfo "Waiting 2 seconds for Fermentrack to send updates if triggered from the web..."
 sleep 2s
 
 # Next, kill the running Fermentrack instance using circus
-echo "Stopping circus..."
-circusctl stop &> /dev/null
+printinfo "Stopping circus..."
+circusctl stop &> upgrade.log
 
 # Pull the latest version of the script from GitHub
-echo "Updating from git..."
+printinfo "Updating from git..."
 cd ~/fermentrack  # Assuming the directory based on a normal install with Fermentrack-tools
-git fetch &> /dev/null
-git reset --hard &> /dev/null
+git fetch &> upgrade.log
+git reset --hard &> upgrade.log
 git checkout ${BRANCH}
-git pull &> /dev/null
+git pull &> upgrade.log
 
 # Install everything from requirements.txt
-echo "Updating requirements via pip..."
-pip install -r requirements.txt --upgrade
+printinfo "Updating requirements via pip..."
+pip install -r requirements.txt --upgrade &> upgrade.log
 
 # Migrate to create/adjust anything necessary in the database
-echo "Running manage.py migrate..."
-python manage.py migrate
+printinfo "Running manage.py migrate..."
+python manage.py migrate &> upgrade.log
 
 # Migrate to create/adjust anything necessary in the database
-echo "Running manage.py collectstatic..."
+printinfo "Running manage.py collectstatic..."
 python manage.py collectstatic --noinput >> /dev/null
 
 
 # Finally, relaunch the Fermentrack instance using circus
-echo "Relaunching circus..."
-circusctl reloadconfig &> /dev/null
-circusctl start &> /dev/null
-echo "Complete!"
+printinfo "Relaunching circus..."
+circusctl reloadconfig &> upgrade.log
+circusctl start &> upgrade.log
+printinfo "Complete!"
