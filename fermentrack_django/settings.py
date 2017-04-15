ac2bcffabe7d3c45524eecd382a12ce32c9b00ec
Thorrak <thorrak@excelinspace.com>
2017-04-15 10:47:43 -0400

    Add requisite entries for celery beat support

--- fermentrack_django/settings.py
+++ fermentrack_django/settings.py
@@ -187,3 +187,12 @@ MESSAGE_TAGS = {
 LOGIN_URL = 'login'              # Used in @login_required decorator
 CONSTANCE_SETUP_URL = 'setup_config'    # Used in @site_is_configured decorator
 
+
+# Celery Configuration
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'
+
+#: Only add pickle to this list if your broker is secured
+#: from unwanted access (see userguide/security.html)
+CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+CELERY_TASK_SERIALIZER = 'json'
