539a7522ecdce5197b0464155a95693de40ac405
Thorrak <thorrak@excelinspace.com>
2017-06-25 09:56:53 -0400

    Helps if I pay attention to my object model

--- app/models.py
+++ app/models.py
@@ -1913,7 +1913,8 @@ class OldControlConstants(models.Model):
 
         if prior_control_constants is None:
             # Load the preexisting control constants from the controller
-            prior_control_constants = OldControlConstants().load_from_controller(controller)
+            prior_control_constants = OldControlConstants()
+            prior_control_constants.load_from_controller(controller)
 
         for this_field in self.firmware_field_list:
             # Now loop through and check each field to find out what changed
