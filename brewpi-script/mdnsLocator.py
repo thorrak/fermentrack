53d6768550558803166629764da30e3faf0a7242
Thorrak <thorrak@excelinspace.com>
2017-08-20 18:14:39 -0400

    Add standalone scanning functionality to mdnsLocator script

--- brewpi-script/mdnsLocator.py
+++ brewpi-script/mdnsLocator.py
@@ -1,5 +1,12 @@
+# This is the same exact file as brewpi-script/mdnsLocator.py
+
 import zeroconf
 from time import sleep
+try:
+    from models import BrewPiDevice
+    djangoLoaded = True
+except:
+    djangoLoaded = False
 
 
 class zeroconfListener(object):
@@ -20,8 +27,6 @@ class zeroconfListener(object):
 
 
 def locate_brewpi_services():
-    # This error was fixed in a recent version of zeroconf
-    # print("NOTE - An error message from 'zeroconf' may appear below. Ignore it - it is working as intended.")
     zeroconf_obj = zeroconf.Zeroconf()
     listener = zeroconfListener()
     browser = zeroconf.ServiceBrowser(zeroconf_obj, "_brewpi._tcp.local.", listener)
@@ -32,11 +37,50 @@ def locate_brewpi_services():
     return listener.brewpi_services
 
 
+def find_mdns_devices():
+    services = locate_brewpi_services()
+
+    installed_devices = []
+    available_devices = []
+    found_device = {}
+
+    for this_service in services:
+        found_device['mDNSname'] = services[this_service].server[:-1]
+        found_device['board'] = services[this_service].properties['board']
+        found_device['branch'] = services[this_service].properties['branch']
+        found_device['revision'] = services[this_service].properties['revision']
+        found_device['version'] = services[this_service].properties['version']
+
+        if djangoLoaded:  # Breaking this out so that we can have this be a direct clone for brewpi-script
+            try:
+                # If we found the device, then we're golden - it's already installed (in theory)
+                found_device['device'] = BrewPiDevice.objects.get(wifi_host=found_device['mDNSname'])
+                installed_devices.append(found_device.copy())
+            except:
+                found_device['device'] = None
+                available_devices.append(found_device.copy())
+        else:
+            found_device['device'] = None
+            available_devices.append(found_device.copy())
+
+    return installed_devices, available_devices
+
+
 if __name__ == '__main__':
-    zeroconf_obj = zeroconf.Zeroconf()
-    listener = zeroconfListener()
-    browser = zeroconf.ServiceBrowser(zeroconf_obj, "_brewpi._tcp.local.", listener)
-    try:
-        input("Press enter to exit...\n\n")
-    finally:
-        zeroconf_obj.close()
+    # zeroconf_obj = zeroconf.Zeroconf()
+    # listener = zeroconfListener()
+    # browser = zeroconf.ServiceBrowser(zeroconf_obj, "_brewpi._tcp.local.", listener)
+
+    print "Scanning for available mDNS devices"
+    _, available_devices = find_mdns_devices()
+
+    for this_device in available_devices:
+        print "Found Device: {} - Board {} - Branch {} - Revision {}".format(this_device['mDNSname'],
+                                                                             this_device['board'],
+                                                                             this_device['branch'],
+                                                                             this_device['revision'])
+    print "All found devices listed. Exiting."
+    # try:
+    #     input("Press enter to exit...\n\n")
+    # finally:
+    #     zeroconf_obj.close()
