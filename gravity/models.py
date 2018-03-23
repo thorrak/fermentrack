299b3a17bd8acbfd79b9a0bbcb54754f9f6b101d
Thorrak <thorrak@excelinspace.com>
2018-03-23 07:06:22 -0400

    Tweak to shorttext (#246)

--- gravity/models.py
+++ gravity/models.py
@@ -372,13 +372,17 @@ class GravityLogPoint(models.Model):
             # Annotations are just the extra data (for now)
             retval = []
             if self.extra_data is not None:
-                if isinstance(self.extra_data, basestring):
-                    shortText = self.extra_data[:1]
-                else:
-                    # If the extra_data isn't a string (the angle that we're saving for iSpindels, for example) then
-                    # we need to manually determine the shortText.
-                    # That said, I could argue that we shouldn't be saving anything out at all (annotation wise) if
-                    # this is the case. Question for later.
+                # TODO - Fix this
+                try:
+                    if isinstance(self.extra_data, basestring):
+                        shortText = self.extra_data[:1]
+                    else:
+                        # If the extra_data isn't a string (the angle that we're saving for iSpindels, for example) then
+                        # we need to manually determine the shortText.
+                        # That said, I could argue that we shouldn't be saving anything out at all (annotation wise) if
+                        # this is the case. Question for later.
+                        shortText = "f"
+                except:
                     shortText = "f"
                 retval.append({'series': 'temp', 'x': time_value, 'shortText': shortText,
                                'text': self.extra_data})
