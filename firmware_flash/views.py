11a673170d28712a483ee8081ee9409d6ad36557
Thorrak <thorrak@excelinspace.com>
2018-03-24 18:19:47 -0400

    Refactor out render_with_devices

--- firmware_flash/views.py
+++ firmware_flash/views.py
@@ -29,20 +29,6 @@ except:
     FERMENTRACK_INTEGRATION = False
 
 
-def render(request, template_name, context=None, content_type=None, status=None, using=None):
-    all_devices = BrewPiDevice.objects.all()
-
-    if context:  # Append to the context dict if it exists, otherwise create the context dict to add
-        context['all_devices'] = all_devices
-    else:
-        context={'all_devices': all_devices}
-
-    return render(request, template_name, context, content_type, status, using)
-
-
-
-
-
 @login_required
 @site_is_configured
 def firmware_select_family(request):
