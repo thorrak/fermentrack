69d51d877814c8efeb09f589b53a734133967ea5
Thorrak <thorrak@excelinspace.com>
2018-04-07 20:07:01 -0400

    Add raven support

--- fermentrack_django/settings.py
+++ fermentrack_django/settings.py
@@ -252,16 +252,16 @@ HUEY = {
 }
 
 
-if config.RAVEN_ENABLED:
-    import raven
+# if config.RAVEN_ENABLED:
+import raven
 
-    RAVEN_CONFIG = {
-        'dsn': 'http://3a1cc1f229ae4b0f88a4c6f7b5d8f394:c10eae5fd67a43a58957887a6b2484b1@sentry.optictheory.com:9000/2',
-        # If you are using git, you can also automatically configure the
-        # release based on the git info.
-        'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
-    }
+RAVEN_CONFIG = {
+    'dsn': 'http://3a1cc1f229ae4b0f88a4c6f7b5d8f394:c10eae5fd67a43a58957887a6b2484b1@sentry.optictheory.com:9000/2',
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+}
 
-    INSTALLED_APPS += (
-        'raven.contrib.django.raven_compat',
-    )
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
