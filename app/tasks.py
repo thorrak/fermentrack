f59e408d2fb9b514c4762801f4428883bb101845
John <thorrak@users.noreply.github.com>
2019-04-21 17:53:17 -0400

    Update for huey 2.0

--- app/tasks.py
+++ app/tasks.py
@@ -5,16 +5,16 @@ from huey.contrib.djhuey import periodic_task, task, db_periodic_task, db_task
 # Note - Huey is being installed/set up, but the functionality will remain latent for the time being. The plan is to
 # use it to create tasks later that run independent from user input (such as controller montioring, etc.)
 
-@task
-def add(x, y):
-    return x + y
-
-
-@task
-def mul(x, y):
-    return x * y
-
-
-@task
-def xsum(numbers):
-    return sum(numbers)
+# @task
+# def add(x, y):
+#     return x + y
+#
+#
+# @task
+# def mul(x, y):
+#     return x * y
+#
+#
+# @task
+# def xsum(numbers):
+#     return sum(numbers)
