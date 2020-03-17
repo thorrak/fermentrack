29c2c512a8d01ad6e4223ff1cb71b80d058876ca
Eddie G <eddieg05@msn.com>
2020-03-17 00:53:31 -0400

    Redirect User upload Image if none available
    Used onerror event to redirect User Logo if not yet uploaded to a default image.
    
    Made some tweaks to settings and urls in an attempt to solve the image url pattern not working on the flat_ui_template. (changes didnt break anything, so were kept)
    
    tThe need for Pillow installation was added to the requirement texts.

--- fermentrack_django/settings.py
+++ fermentrack_django/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                #'django.template.context_processors.media',
+                'django.template.context_processors.media',
                 'constance.context_processors.config',
                 'app.context_processors.preferred_tz',
                 'app.context_processors.devices',
