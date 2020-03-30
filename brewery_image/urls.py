032e689a32e612983652bb8515231c5db8706249
Eddie G <eddieg05@msn.com>
2020-03-29 21:27:22 -0400

    Create link to FermentrackIP/image
    Give users some direction concerning uploading images into Fermentrack
    
    Created Link to the FermentrackIP/image page. If user hasn't uploaded an image, the 'image' page will tell a user how to do it.
    
    (some general clean-up and back-end comments)

--- brewery_image/urls.py
+++ brewery_image/urls.py
@@ -13,5 +13,5 @@ import brewery_image.views
 
 brewery_image_urlpatterns = [
     ## Brewery Image Views (I kinda have an idea what I'm doing)
-    url(r'^image/$', brewery_image.views.display_brewery_images, name='brewery_images'),
+    url(r'^image/$', brewery_image.views.display_brewery_images, name='images'),
 ]
