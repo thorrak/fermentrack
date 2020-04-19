6c097717357fa03f3b1074b19a037d416226ea1f
Thorrak <thorrak@excelinspace.com>
2020-04-19 17:36:14 -0400

    Explicitly find and correct tables with broken foreign key references (Fixes #450)

--- app/management/commands/fix_sqlite_for_django_2.py
+++ app/management/commands/fix_sqlite_for_django_2.py
@@ -7,7 +7,7 @@ from django.db import connection
 from constance import config
 import shutil
 
-from django.db import utils
+import django.db.utils
 
 
 class Command(BaseCommand):
@@ -37,8 +37,35 @@ class Command(BaseCommand):
         # (db) -> old
         shutil.copyfile("db.sqlite3", "db.sqlite3.old")
 
+        # First thing's first - We want to find any broken foreign key checks, and rebuild those tables specifically
+        fixed_tables = []
+        fk_violations = connection.cursor().execute('PRAGMA foreign_key_check').fetchall()
+        # See https://www.sqlite.org/pragma.html#pragma_foreign_key_check
+        for table_name, rowid, referenced_table_name, foreign_key_index in fk_violations:
+            for app in apps.get_app_configs():
+                for model in app.get_models(include_auto_created=True):
+                    if model._meta.managed and not (model._meta.proxy or model._meta.swapped):
+                        if model._meta.db_table == table_name:
+                            # What we're trying to do is check every model in every app to see if the table that holds
+                            # it is the table with the broken foreign key
+                            with connection.schema_editor() as editor:
+                                if table_name not in fixed_tables:
+                                    print("Table {} has a foreign key issue referencing table {} - Attempting to fix...".format(table_name, referenced_table_name))
+                                    try:
+                                        constraint_check = connection.disable_constraint_checking()
+                                    except:
+                                        connection.connection = connection.connect()
+                                        constraint_check = connection.disable_constraint_checking()
+                                    editor._remake_table(model)
+                                    fixed_tables.append(table_name)
+            if table_name not in fixed_tables:
+                print("Unable to locate suitable model referencing table {}. Stopping.".format(table_name))
+                raise django.db.utils.IntegrityError("fix_sqlite_for_django_2 unable to find suitable model referencing table {}".format(table_name))
+
+        # Once that's done, we're going to attempt to loop over all the apps/models and rebuild everything just to be
+        # safe.
         for app in apps.get_app_configs():
-            print("Fixing app {}...".format(app.verbose_name))
+            print("Rebuilding app {}...".format(app.verbose_name))
             for model in app.get_models(include_auto_created=True):
                 if model._meta.managed and not (model._meta.proxy or model._meta.swapped):
                     for base in model.__bases__:
@@ -52,11 +79,7 @@ class Command(BaseCommand):
                             connection.connection = connection.connect()
                             constraint_check = connection.disable_constraint_checking()
                         print("Rebuilding model {} - FK Check Disabled: {}".format(model, constraint_check))
-                        try:
-                            editor._remake_table(model)
-                        except utils.IntegrityError:
-                            # Foreign key check fails during rebuild
-                            pass
+                        editor._remake_table(model)
         connection.check_constraints()
         config.SQLITE_OK_DJANGO_2 = True
         return True
