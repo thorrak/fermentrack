66aaab170a29ee0c7da5e87cf4e3aaf48128596c
John <jbeeler@optictheory.com>
2022-03-27 00:26:20 -0400

    Hide log messages

--- brewpi-script/fermentrack_caller.py
+++ brewpi-script/fermentrack_caller.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
                 processes_to_delete.append(this_process)
         for this_process in processes_to_delete:
             # Do this as step 2 since we can't change the process list mid-iteration
-            print(f"Deleting process for BrewPiDevice #{this_process}")
+            # print(f"Deleting process for BrewPiDevice #{this_process}")
             del process_list[this_process]
 
         active_device_ids = get_active_brewpi_devices()
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         for this_id in active_device_ids:
             if this_id not in process_list:
                 # The process hasn't been spawned. Spawn it.
-                print(f"Launching process for BrewPiDevice #{this_id}")
+                # print(f"Launching process for BrewPiDevice #{this_id}")
                 brewpi_config = FermentrackBrewPiScriptConfig(brewpi_device_id=this_id)
                 brewpi_config.load_from_fermentrack(False)
                 process_list[this_id] = Process(target=BrewPiScript, args=(brewpi_config, ))
